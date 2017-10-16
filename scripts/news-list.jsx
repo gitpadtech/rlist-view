9671f44069b2cca84c99f0f4fc585c7a48ecc457
yeluoqiuzhi <827884374@qq.com>
2017-10-17 01:56:41 +0800

    infinite loading

--- scripts/news-list.jsx
+++ scripts/news-list.jsx
@@ -19,12 +19,16 @@ export default class NewsList extends Component {
       list: []
     }
     this.refresh = this.refresh.bind(this);
+    this.loadMore = this.loadMore.bind(this);
   }
   componentDidMount() {
   }
-  refresh() {
+  fetch() {
     return fetch('list.json')
-      .then(res => res.json())
+    .then(res => res.json());
+  }
+  refresh() {
+    return this.fetch()
       .then(res => {
         return new Promise((resolve, reject) => {
           setTimeout(() => resolve(res), 1000);
@@ -37,6 +41,20 @@ export default class NewsList extends Component {
         });
       });
   }
+  loadMore() {
+    return this.fetch()
+      .then(res => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => resolve(res), 1000);
+        });
+      })
+      .then(res => {
+        res.forEach(raw => raw.id = shortid.generate());
+        this.setState({
+          list: this.state.list.concat(shuffle(res))
+        });
+      });
+  }
   render() {
     const state = this.state;
     return (
@@ -45,6 +63,7 @@ export default class NewsList extends Component {
           refreshComponent={SpinnerRefresh}
           height={window.innerHeight}
           refresh={this.refresh}
+          loadMore={this.loadMore}
         >
           {
             state.list.map(item =>
